88adf2e1e8c91ad0611738bcef987e0b6f73c7e2
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-02-21 22:43:38 +0100

    fix: fetch data just once

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ export default function Home() {
   const { posts, fetchPosts } = useStore()
 
   useEffect(() => {
-    fetchPosts()
-  }, [])
+    if (posts.length === 0) {
+      fetchPosts()
+    }
+  }, [posts, fetchPosts])
 
 
   return (
@@ -17,10 +19,10 @@ export default function Home() {
     >
       <PostList posts={posts} />
       <footer className="row-start-3 flex flex-row gap-6 flex-wrap items-center justify-center">
-          <a href="https://www.linkedin.com/in/hosseinisanaz/" target="_blank" rel="noopener noreferrer">
-            © 2025 Powered by <b>Sanaz</b>
-          </a>
-          <Image src="/logo.png" alt="Anydesk Logo" width={72} height={18} />
+        <a href="https://www.linkedin.com/in/hosseinisanaz/" target="_blank" rel="noopener noreferrer">
+          © 2025 Powered by <b>Sanaz</b>
+        </a>
+        <Image src="/logo.png" alt="Anydesk Logo" width={72} height={18} />
       </footer>
     </div>
   );
